db8ed392bde5e5e158911416df8d002b6df658db
zain-anwer <zain.anwer192005@gmail.com>
2026-03-29 03:36:23 +0500

    Changed sameSite to none in token generation

--- chat_backend/src/lib/utils.js
+++ chat_backend/src/lib/utils.js
@@ -11,7 +11,7 @@ export const generateToken = (userId,res) =>
         {
             maxAge: 7 * 24 * 60 * 60 * 1000,
             httpOnly: true,
-            sameSite: "strict",
+            sameSite: (process.env.NODE_ENV === "development") ? "strict" : "none",
             secure: (process.env.NODE_ENV === "development") ? false : true        
         }
     );
