128a0cd3ba5629ed1412ef7eeed21b3f5422d1e9
zain-anwer <zain.anwer192005@gmail.com>
2026-03-29 01:55:33 +0500

    Added .env for frontend

--- chat_frontend/src/lib/axios.js
+++ chat_frontend/src/lib/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export const axiosInstance = axios.create(
     {
-        baseURL: "http://localhost:3000/api",
+        baseURL: import.meta.env.VITE_BACKEND_URL,
         withCredentials:true
     }
 )
--- chat_frontend/src/lib/socket.js
+++ chat_frontend/src/lib/socket.js
@@ -1,6 +1,6 @@
 import {io} from 'socket.io-client'
 
-const BASE_URL = "http://localhost:3000" 
+const BASE_URL = import.meta.env.VITE_BACKEND_URL
 
 
 export const socketInstance = io(
