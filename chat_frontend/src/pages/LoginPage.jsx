0856a613f9e8577cd4291477d38edacb6e79741b
zain-anwer <zain.anwer192005@gmail.com>
2026-06-14 13:06:32 +0500

    Changed placeholder values for auth pages

--- chat_frontend/src/pages/LoginPage.jsx
+++ chat_frontend/src/pages/LoginPage.jsx
@@ -67,7 +67,7 @@ const LoginPage = ({setAuth})=>{
                         <label>Email
                             <div className="input-group">
                                 <Mail/>
-                                <input type="email" placeholder="spongebob.squarepants@gmail.com"
+                                <input type="email" placeholder="spongebob@gmail.com"
                                     value={formData.email} onChange={(e)=>{setFormData({...formData,email:e.target.value})}}
                                 />
                             </div>
